ea1f2a2b44edfdd31d3a1e7332d98b09340a91c1
Leighton Chen <lec018@ucsd.edu>
2016-10-01 14:19:27 -0700

    FUCKING SETTINGS

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require('express'),
 	app = express(),
 	morgan = require('morgan'),
 	bodyParser = require('body-parser'),
-	moment = require('moment');
+	moment = require('moment'),
+	formidable = require("formidable");
 
 app.use(express.static(__dirname + '/public'));                 // static files
 app.use(morgan('dev'));                                         // log every request to the console
@@ -12,9 +13,12 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 
 // Server start-up
-var server = app.listen(8080, function () {
+var server = app.listen(8080, function (req, res) {
   console.log("Listening on port %s", server.address().port);
 });
+
+// Variables
+var height = 0;
 
 // Routes
 app.get('/', function (req, res) {
@@ -36,3 +40,18 @@ app.get('/settings', function(req,res) {
     if (!res.headersSent)
     	res.sendFile(__dirname + "/public/settings.html");
 });
+
+function processAllFieldsOfForm(req,res) {
+	var form = new formidable.IncomingForm();
+
+	form.parse(req, function (err, fields, files) {
+		res.writeHead(200, {
+			'content-type': 'text/plain'
+		});
+		res.write('received the data:\n\n');
+		res.end(util.inspect({
+			fields:fields,
+			files: files
+		}));
+	});
+}
